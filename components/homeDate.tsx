c5b23a4508249f29d25aa8116f9fa28798dc1a41
sy1020 <ohseyeong041020@gmail.com>
2025-11-27 16:45:38 +0900

    modify Dates

--- components/homeDate.tsx
+++ components/homeDate.tsx
@@ -3,20 +3,35 @@ import { StyleSheet, Text, View } from 'react-native';
 const DATE_SIZE = 30;
 
 export default function HomeDate(){
-    const dates = [5, 6, 7, 8, 9, 10, 11]; //오늘로부터 6일전부터 출력
-    const activeDates = [9, 10, 11]; // 접속한 날짜
-    const currentDate = 11; //오늘은 11일
+    // const activeDates = [9, 10, 11]; // 접속한 날짜
+    // const dates = [5, 6, 7, 8, 9, 10, 11]
 
+    const getPastWeek = () => {
+        const tempDates = [];
+        const today = new Date();
+
+        for (let i = 6; i>=0; i--){
+            const d = new Date(today); // d는 년, 월, 일 다 저장되어 있음
+            d.setDate(today.getDate()-i);
+            tempDates.push(d.getDate());
+        }
+        
+        return tempDates;
+    }
+
+    const dates = getPastWeek();
+    const currentDate = new Date().getDate(); //오늘 날짜
+    const activeDates = [dates[0], dates[4], dates[5], dates[6]]; // 임시로 활동 일 해두기
     return (
         <View style={styles.dateContainer}>
             <View style={styles.line} />
             <View style={styles.dateRow}>
-                {dates.map((date)=> {
+                {dates.map((date, index)=> {
                     const isActive = activeDates.includes(date);
                     const isCurrent = date === currentDate;
 
                     return(
-                        <View key={date} style={styles.dateItem}>
+                        <View key={index} style={styles.dateItem}>
                             <View style={[
                                 styles.dateCircle,
                                 isActive && styles.activeDateCircle
